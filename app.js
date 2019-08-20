6686ef6702174a939c3317f3526338aaf8a2fd6d
robert vanarsdall <robv@roberts-MacBook-Pro.local>
2019-08-20 18:40:59 -0400

    app.js init

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
 
 var express = require('express');
 var app = express();
+var user= require('./controllers/usercontroller')
+var walkerRequest= require('./controllers/walkercontroller')
+var dogOwner = require('./controllers/ownercontroller')
+
+var fakeDataCreating = require('./controllers/databasedatacontroller')
+
+var sequelize = require('./db')
+var bodyParser = require('body-parser');
+
+
+//sequelize.sync()
+
+app.use(bodyParser.json())
+
+app.use(require('./middleware/headers'))
+
+app.use('/user', user )  //Handling User signup/ login
+
+app.use('/create', fakeDataCreating) //used for creating fake data in database for demo mode
+
+// app.use(require('./middleware/validate-session'))
+
+app.use('/walker', walkerRequest) // used for walker request
+
+app.use('/owner',dogOwner) // adding pets based on dog owners
 
 
 
