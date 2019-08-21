98ba64c6ca017cffc32402392f4e8e3e44b5667d
robert vanarsdall <robv@roberts-MacBook-Pro.local>
2019-08-21 19:45:33 -0400

    walker-controller

--- controllers/walkercontroller.js
+++ controllers/walkercontroller.js
@@ -1,5 +1,29 @@
 
+var express = require('express')
+var router = express.Router()
+var sequelize = require('../db')
 
+var petTable = sequelize.import('../models/petTable')
+var serviceRequestTable = sequelize.import('../models/serviceRequestTable')
+
+
+
+
+// CreateRequest(/create)
+// Get SearchRequest(/find) - search the userTable with zip code nearby that are petWalker or both make sure it’s not sure owner populates on there
+// update(/booked) - Found a provider and would like to book them - We need to update the PetRequest Row with the Petwalker’s UserID and changed the isBooked to Yes
+
+
+//PetOwner adding dogs
+
+router.post('/adding-pet', function(req,res){
+    var userid = req.user.id
+    var addingPetData = req.body.data
+
+    petTable.create({
+        
+    })
+})
 
 
 module.exports = router;
