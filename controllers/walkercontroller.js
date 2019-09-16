0f3d957fae6c54dc3e0fad5e3fc1f8233752dd68
robert vanarsdall <robv@roberts-MacBook-Pro.local>
2019-09-16 12:24:58 -0400

    added more routes

--- controllers/walkercontroller.js
+++ controllers/walkercontroller.js
@@ -4,7 +4,7 @@ var sequelize = require("../db");
 var serviceRequestTable = sequelize.import("../models/serviceRequestTable");
 var Auth = sequelize.import("../models/userTable");
 var Sequelize = require("sequelize");
-var reqTable =require('../models/serviceRequestTable')
+var reqTable = require("../models/serviceRequestTable");
 
 router.put("/update", function(req, res) {
   var user = req.user.id;
@@ -71,12 +71,12 @@ router.put("/update-request/:id", function(req, res) {
         dateRequested: updateRequestData.dateRequested,
         timeRequested: updateRequestData.timeRequested,
         walkerId: updateRequestData.walkerId,
-        userId: userid,
         isAccepted: updateRequestData.isAccepted,
         isCompleted: updateRequestData.isCompleted,
         ownerNotified: updateRequestData.ownerNotified,
         reviewTitle: updateRequestData.reviewTitle,
-        review: updateRequestData.review
+        review: updateRequestData.review,
+        dogs: updateRequestData.dogs
       },
       {
         where: {
@@ -95,12 +95,12 @@ router.put("/update-request/:id", function(req, res) {
 
 //WALKER PENDING REQUESTS
 
-router.get("/pendingRequests/:id", function(req, res) {
-  var requestID = req.params.id;
+router.get("/pending-requests/", function(req, res) {
+  var userID = req.user.id;
 
   serviceRequestTable
     .findAll({
-      where: { walkerId: requestID.toString(), isAccepted: false }
+      where: { walkerId: userID.toString(), isAccepted: false }
     })
     .then(
       function findAllSuccess(data) {
@@ -112,8 +112,8 @@ router.get("/pendingRequests/:id", function(req, res) {
     );
 });
 
-//WALKER REQUESTS ACCEPTED
-router.get("/acceptedRequests/", function(req, res) {
+//WALKER REQUESTS ACCEPTED TABLE
+router.get("/accepted-requests/", function(req, res) {
   var walkerID = req.user.id;
 
   serviceRequestTable
@@ -149,17 +149,76 @@ router.get("/owner-requests/", function(req, res) {
     );
 });
 
+//REVIEW TABLE WALKER DASHBOARD
+
+router.get("/list-reviews-dashboard/", function(req, res) {
+  var userID = req.user.id;
+
+  serviceRequestTable
+    .findAll({
+      where: { userId: userID.toString() }
+    })
+    .then(
+      function findAllSuccess(data) {
+        res.json(data);
+      },
+      function findAllError(err) {
+        res.send(500, err.message);
+      }
+    );
+});
+
 router.get("/test/", function(req, res) {
   // var userID = req.user.id;
 
-  sequelize.query("SELECT * from pets LEFT OUTER JOIN users ON 'pets.userId'='users.id' where 'pets.userId'='5'").then(([results, metadata]) => {
+  sequelize
+    .query(
+      "SELECT * from pets LEFT OUTER JOIN users ON 'pets.userId'='users.id' where 'pets.userId'='5'"
+    )
+    .then(
+      ([results, metadata]) => {
+        res.json(results);
+        // res.json(metadata)
+        // Results will be an empty array and metadata will contain the number of affected rows.
+      },
+      function findAllError(err) {
+        res.send(500, err);
+      }
+    );
+});
 
-    res.json(results)
-    // res.json(metadata)
-    // Results will be an empty array and metadata will contain the number of affected rows.
-  }, function findAllError(err) {
-    res.send(500, err);
-  })
+// ANYTHING WALKER UPDATES
+router.put("/walker-update-request/:id", function(req, res) {
+  var userid = req.user.id;
+  var updateRequestData = req.body.data;
+  var requestID = req.params.id;
+
+  serviceRequestTable
+    .update(
+      {
+        dateRequested: updateRequestData.dateRequested,
+        timeRequested: updateRequestData.timeRequested,
+        walkerId: updateRequestData.walkerId,
+        isAccepted: updateRequestData.isAccepted,
+        isCompleted: updateRequestData.isCompleted,
+        ownerNotified: updateRequestData.ownerNotified,
+        reviewTitle: updateRequestData.reviewTitle,
+        review: updateRequestData.review,
+        dogs: updateRequestData.dogs
+      },
+      {
+        where: {
+          id: requestID,
+          walkerId: userid.toString()
+        }
+      }
+    )
+    .then(
+      function updateSuccess(updatedService) {
+        res.json(updatedService);
+      },
+      err => res.send(500, err.message)
+    );
 });
 
 module.exports = router;
--- models/serviceRequestTable.js
+++ models/serviceRequestTable.js
@@ -1,38 +1,40 @@
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define("service-request", {
-
-      dateRequested: {
-        type: DataTypes.STRING
-      },
-      timeRequested: {
-        type: DataTypes.STRING
-      },
-      walkerId: {
-        type: DataTypes.STRING
-      },
-      userId: {
-        type: DataTypes.STRING
-      },
-      isAccepted: {
-        type: DataTypes.BOOLEAN,
-        default: false
-      },
-      isCompleted: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-      },
-      ownerNotified: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-      },
-      reviewTitle: {
-        type: DataTypes.STRING
-      },
-      review: {
-        type: DataTypes.STRING(20000)
-      },
-      rating: {
-        type: DataTypes.STRING
-      }
-    });
-  };
+  return sequelize.define("service-request", {
+    dateRequested: {
+      type: DataTypes.STRING
+    },
+    timeRequested: {
+      type: DataTypes.STRING
+    },
+    walkerId: {
+      type: DataTypes.STRING
+    },
+    userId: {
+      type: DataTypes.STRING
+    },
+    isAccepted: {
+      type: DataTypes.BOOLEAN,
+      default: false
+    },
+    isCompleted: {
+      type: DataTypes.BOOLEAN,
+      default: false
+    },
+    ownerNotified: {
+      type: DataTypes.BOOLEAN,
+      default: false
+    },
+    reviewTitle: {
+      type: DataTypes.STRING
+    },
+    review: {
+      type: DataTypes.STRING(20000)
+    },
+    rating: {
+      type: DataTypes.STRING
+    },
+    dogs: {
+      type: DataTypes.STRING
+    }
+  });
+};
