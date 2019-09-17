7496a4f7c860ce923b5175bf68b6461010602367
robert vanarsdall <Rob@roberts-Mac.local>
2019-09-16 23:23:45 -0400

    updated user table model to reflect lat lng for google maps api

--- controllers/ownercontroller.js
+++ controllers/ownercontroller.js
@@ -1,31 +1,28 @@
-var router = require('express').Router();
-var sequelize = require('../db');
-var petTable = sequelize.import('../models/petTable');
-var Auth = sequelize.import('../models/userTable');
+var router = require("express").Router();
+var sequelize = require("../db");
+var petTable = sequelize.import("../models/petTable");
+var Auth = sequelize.import("../models/userTable");
 
-
-
-router.get('/userinfo', function(req, res) {
-    Auth.findOne( { where: {id: req.user.id }}).then(
-        function(data) {
-
-            res.json({
-                data: data
-            });
-        },
-            function (err) {
-                res.status(501).send({error: 'Data Error'});
-            }
-    );
+router.get("/userinfo", function(req, res) {
+  Auth.findOne({ where: { id: req.user.id } }).then(
+    function(data) {
+      res.json({
+        data: data
+      });
+    },
+    function(err) {
+      res.status(501).send({ error: "Data Error" });
+    }
+  );
 });
 
-router.put('/address', function(req, res) {
-    var user = req.user.id;
-    var ownerData = req.body.data;
-  
-    Auth
-    .update({
-      address:ownerData.street,
+router.put("/address", function(req, res) {
+  var user = req.user.id;
+  var ownerData = req.body.data;
+
+  Auth.update(
+    {
+      address: ownerData.street,
       city: ownerData.city,
       state: ownerData.state,
       zip: ownerData.zipcode,
@@ -33,113 +30,117 @@ router.put('/address', function(req, res) {
       bio: ownerData.bio,
       pic: ownerData.picture,
       rating: ownerData.rating,
-      numberOfWalks: ownerData.numberOfWalks
+      numberOfWalks: ownerData.numberOfWalks,
+      lat: ownerData.lat,
+      lng: ownerData.lng
     },
-    {where: {id: user}}
-    ).then(
-        function updateSuccess(updatedLog) {
-            res.json({
-                data: updatedLog
-            });
-        },
-        function updateError(err){
-            res.send(500, err.message);
-        }
-    )
-    });
+    { where: { id: user } }
+  ).then(
+    function updateSuccess(updatedLog) {
+      res.json({
+        data: updatedLog
+      });
+    },
+    function updateError(err) {
+      res.send(500, err.message);
+    }
+  );
+});
 
+router.post("/create", function(req, res) {
+  var owner = req.user.id;
+  var petName = req.body.data.petName;
+  var image = req.body.data.image;
+  var breed = req.body.data.breed;
+  var age = req.body.data.age;
+  var weight = req.body.data.weight;
+  var gender = req.body.data.gender;
+  var bio = req.body.data.bio;
 
-router.post('/create', function (req, res) {
-    var owner = req.user.id;
-    var petName = req.body.data.petName;
-    var image = req.body.data.image;
-    var breed = req.body.data.breed;
-    var age = req.body.data.age;
-    var weight = req.body.data.weight;
-    var gender = req.body.data.gender;
-    var bio = req.body.data.bio;
-
-    petTable
+  petTable
     .create({
-        userId: owner,
-        petName: petName,
-        petPic: image,
-        breed: breed,
-        age: age,
-        weight: weight,
-        gender: gender,
-        bio: bio
+      userId: owner,
+      petName: petName,
+      petPic: image,
+      breed: breed,
+      age: age,
+      weight: weight,
+      gender: gender,
+      bio: bio
     })
     .then(
-        function createSuccess(pets) {
-            res.json({
-                pets: pets
-            });
-        },
-        function createError(err) {
-            res.send(500, err.message);
-        }
+      function createSuccess(pets) {
+        res.json({
+          pets: pets
+        });
+      },
+      function createError(err) {
+        res.send(500, err.message);
+      }
     );
 });
 
-router.get('/', function(req, res) {
-    var data = req.user.id;
+router.get("/", function(req, res) {
+  var data = req.user.id;
 
-    petTable
+  petTable
     .findAll({
-        where: { userId: data.toString() }
-    }).then(
-        function findAllSuccess(data) {
-            res.json(data);
-        },
-        function findAllError(err) {
-            res.send(500, err.message);
-        }
+      where: { userId: data.toString() }
+    })
+    .then(
+      function findAllSuccess(data) {
+        res.json(data);
+      },
+      function findAllError(err) {
+        res.send(500, err.message);
+      }
     );
 });
 
-router.delete('/delete/:id', function(req, res) {
-    var data = req.params.id;
-    var userId = req.user.id;
+router.delete("/delete/:id", function(req, res) {
+  var data = req.params.id;
+  var userId = req.user.id;
 
-
-    petTable
+  petTable
     .destroy({
-        where: { id: data, userId: userId.toString() }
-    }).then(
-        function deleteLogSuccess(data){
-            res.status(200).json({message: 'deleted'});
-        },
-        function deleteLogError(err){
-            res.status(500).json({message: err.message});
-        }
+      where: { id: data, userId: userId.toString() }
+    })
+    .then(
+      function deleteLogSuccess(data) {
+        res.status(200).json({ message: "deleted" });
+      },
+      function deleteLogError(err) {
+        res.status(500).json({ message: err.message });
+      }
     );
 });
 
-router.put('/update/:id', function(req, res) {
-    var data = req.params.id;
-    var pets = req.body.data;
+router.put("/update/:id", function(req, res) {
+  var data = req.params.id;
+  var pets = req.body.data;
 
-    petTable
-    .update({
+  petTable
+    .update(
+      {
         petName: pets.pets,
         petPic: pets.petPic,
         breed: pets.breed,
         age: pets.age,
         weight: pets.weight,
-        gender: pets.gender,
-    },
-    {where: {id: data}}
-    ).then(
-        function updateSuccess(updatedLog) {
-            res.json({
-                petName: pets
-            });
-        },
-        function updateError(err){
-            res.send(500, err.message);
-        }
+        gender: pets.gender
+      },
+      { where: { id: data } }
     )
-    });
+    .then(
+      function updateSuccess(updatedLog) {
+        res.json({
+          petName: pets
+        });
+      },
+      function updateError(err) {
+        res.send(500, err.message);
+      }
+    );
+});
 
 module.exports = router;
