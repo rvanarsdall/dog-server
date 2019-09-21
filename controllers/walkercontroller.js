1d07a92abccb7940f235a660b4ea680e807ec341
Tracy Smart <tracysmart@Tracys-MacBook-Pro.local>
2019-09-21 10:44:16 -0400

    walker controller

--- controllers/walkercontroller.js
+++ controllers/walkercontroller.js
@@ -5,11 +5,9 @@ var serviceRequestTable = sequelize.import("../models/serviceRequestTable");
 var Auth = sequelize.import("../models/userTable");
 var Sequelize = require("sequelize");
 var reqTable = require("../models/serviceRequestTable");
-
 router.put("/update", function(req, res) {
   var user = req.user.id;
   var ownerData = req.body.data;
-
   Auth.update(
     {
       address: ownerData.street,
@@ -35,16 +33,16 @@ router.put("/update", function(req, res) {
   );
 });
 //Request Create
-
 router.post("/create-request", function(req, res) {
   var userid = req.user.id;
   var addingRequestData = req.body.data;
-
   serviceRequestTable
     .create({
       dateRequested: addingRequestData.dateRequested,
       timeRequested: addingRequestData.timeRequested,
-      userId: userid
+      userid: userid,
+      walkerid: addingRequestData.walkerID,
+      dogs: addingRequestData.dogs
     })
     .then(
       function createSuccess(data) {
@@ -57,31 +55,44 @@ router.post("/create-request", function(req, res) {
       }
     );
 });
-
+//DELETING REQUEST
+router.delete("/delete/:id", function(req, res) {
+  var data = req.params.id;
+  var userid = req.user.id;
+  serviceRequestTable
+    .destroy({
+      where: { id: data, userid: userid }
+    })
+    .then(
+      function deleteLogSuccess(data) {
+        res.status(200).json({ message: "you killed a dog request" });
+      },
+      function deleteLogError(err) {
+        res.status(500).json(err.message);
+      }
+    );
+});
 //request update
-
 router.put("/update-request/:id", function(req, res) {
   var userid = req.user.id;
   var updateRequestData = req.body.data;
   var requestID = req.params.id;
-
-  serviceRequestTab
+  serviceRequestTable
     .update(
       {
         dateRequested: updateRequestData.dateRequested,
         timeRequested: updateRequestData.timeRequested,
-        walkerId: updateRequestData.walkerId,
-        isAccepted: updateRequestData.isAccepted,
-        isCompleted: updateRequestData.isCompleted,
-        ownerNotified: updateRequestData.ownerNotified,
+        walkerid: updateRequestData.walkerId,
+        isaccepted: updateRequestData.isAccepted,
+        iscompleted: updateRequestData.isCompleted,
+        ownernotified: updateRequestData.ownerNotified,
         reviewTitle: updateRequestData.reviewTitle,
-        review: updateRequestData.review,
-        dogs: updateRequestData.dogs
+        review: updateRequestData.review
       },
       {
         where: {
           id: requestID,
-          userId: userid.toString()
+          userid: userid
         }
       }
     )
@@ -89,88 +100,95 @@ router.put("/update-request/:id", function(req, res) {
       function updateSuccess(updatedService) {
         res.json(updatedService);
       },
-      err => res.send(500, err.message)
+      err => res.send(500, err)
     );
 });
-
+router.put("/walker-update-request/:id", function(req, res) {
+  var walkerid = req.user.id;
+  var updateRequestData = req.body.data;
+  var requestID = req.params.id;
+  serviceRequestTable
+    .update(
+      {
+        isaccepted: updateRequestData.isAccepted,
+        iscompleted: updateRequestData.isCompleted,
+        walkerid: updateRequestData.walkerID
+      },
+      {
+        where: {
+          id: requestID
+        }
+      }
+    )
+    .then(
+      function updateSuccess(updatedService) {
+        res.json(updatedService);
+      },
+      err => res.send(500, err)
+    );
+});
 //WALKER PENDING REQUESTS
-
-router.get("/pending-requests/", function(req, res) {
-  var userID = req.user.id;
-
-  serviceRequestTable
-    .findAll({
-      where: { walkerId: userID.toString(), isAccepted: false }
-    })
-    .then(
-      function findAllSuccess(data) {
-        res.json(data);
-      },
-      function findAllError(err) {
-        res.send(500, err.message);
-      }
-    );
-});
-
-//WALKER REQUESTS ACCEPTED
-router.get("/accepted-requests/", function(req, res) {
-  var walkerID = req.user.id;
-
-  serviceRequestTable
-    .findAll({
-      where: { walkerId: walkerID.toString(), isAccepted: true }
-    })
-    .then(
-      function findAllSuccess(data) {
-        res.json(data);
-      },
-      function findAllError(err) {
-        res.send(500, err.message);
-      }
-    );
-});
-
+// router.get("/pending-requests/", function(req, res) {
+//   var requestID = req.user.id;
+//   serviceRequestTable
+//     .findAll({
+//       where: { walkerid: requestID, isAccepted: false }
+//     })
+//     .then(
+//       function findAllSuccess(data) {
+//         res.json(data);
+//       },
+//       function findAllError(err) {
+//         res.send(500, err.message);
+//       }
+//     );
+// });
+// //WALKER REQUESTS ACCEPTED
+// router.get("/accepted-requests/", function(req, res) {
+//   var walkerID = req.user.id;
+//   serviceRequestTable
+//     .findAll({
+//       where: { walkerid: walkerID, isAccepted: true }
+//     })
+//     .then(
+//       function findAllSuccess(data) {
+//         res.json(data);
+//       },
+//       function findAllError(err) {
+//         res.send(500, err.message);
+//       }
+//     );
+// });
 //OWNER REQUEST TABLE
-
 router.get("/owner-requests/", function(req, res) {
-  var userID = req.user.id;
-
-  serviceRequestTable
-    .findAll({
-      where: { userId: userID.toString() }
-    })
+  // var userID = req.user.id;
+  sequelize
+    .query(
+      `SELECT * from users INNER JOIN servicerequests ON servicerequests.walkerid=users.id  where servicerequests.userid=${req.user.id} ORDER BY servicerequests.id`
+    )
     .then(
-      function findAllSuccess(data) {
-        res.json(data);
+      ([results, metadata]) => {
+        res.json(results);
       },
       function findAllError(err) {
-        res.send(500, err.message);
+        res.send(500, err);
       }
     );
+  // serviceRequestTable
+  //   .findAll({
+  //     where: { userid: userID.toString() }
+  //   })
+  //   .then(
+  //     function findAllSuccess(data) {
+  //       res.json(data);
+  //     },
+  //     function findAllError(err) {
+  //       res.send(500, err.message);
+  //     }
+  //   );
 });
-
-//REVIEW TABLE WALKER DASHBOARD
-
-router.get("/list-reviews-dashboard/", function(req, res) {
-  var userID = req.user.id;
-
-  serviceRequestTable
-    .findAll({
-      where: { userid: userID() }
-    })
-    .then(
-      function findAllSuccess(data) {
-        res.json(data);
-      },
-      function findAllError(err) {
-        res.send(500, err.message);
-      }
-    );
-});
-
 router.get("/basic-info/:id", function(req, res) {
   var userID = req.params.id;
-
   Auth.findAll({
     where: { id: userID }
   }).then(
@@ -182,58 +200,38 @@ router.get("/basic-info/:id", function(req, res) {
     }
   );
 });
-
-router.get("/test/", function(req, res) {
+/// WALKER PENDING REQUESTS
+router.get("/pending-requests/", function(req, res) {
   // var userID = req.user.id;
-
+  // walkerid=${req.user.id} AND
   sequelize
     .query(
-      "SELECT * from pets LEFT OUTER JOIN users ON 'pets.userId'='users.id' where 'pets.userId'='5'"
+      `SELECT * from users INNER JOIN servicerequests ON servicerequests.userid=users.id where walkerid=${req.user.id} AND servicerequests.isaccepted=false ORDER BY servicerequests.id`
     )
     .then(
       ([results, metadata]) => {
         res.json(results);
-        // res.json(metadata)
-        // Results will be an empty array and metadata will contain the number of affected rows.
       },
       function findAllError(err) {
         res.send(500, err);
       }
     );
 });
-
-// ANYTHING WALKER UPDATES
-router.put("/walker-update-request/:id", function(req, res) {
-  var userid = req.user.id;
-  var updateRequestData = req.body.data;
-  var requestID = req.params.id;
-
-  serviceRequestTable
-    .update(
-      {
-        dateRequested: updateRequestData.dateRequested,
-        timeRequested: updateRequestData.timeRequested,
-        walkerId: updateRequestData.walkerId,
-        isAccepted: updateRequestData.isAccepted,
-        isCompleted: updateRequestData.isCompleted,
-        ownerNotified: updateRequestData.ownerNotified,
-        reviewTitle: updateRequestData.reviewTitle,
-        review: updateRequestData.review,
-        dogs: updateRequestData.dogs
-      },
-      {
-        where: {
-          id: requestID,
-          walkerId: userid.toString()
-        }
-      }
+///WALKER ACCEPTED REQUESTS
+router.get("/accepted-requests/", function(req, res) {
+  // var userID = req.user.id;
+  // walkerid=${req.user.id} AND
+  sequelize
+    .query(
+      `SELECT * from users INNER JOIN servicerequests ON servicerequests.userid=users.id  where walkerid=${req.user.id} AND servicerequests.isaccepted=true ORDER BY servicerequests.id`
     )
     .then(
-      function updateSuccess(updatedService) {
-        res.json(updatedService);
+      ([results, metadata]) => {
+        res.json(results);
       },
-      err => res.send(500, err.message)
+      function findAllError(err) {
+        res.send(500, err);
+      }
     );
 });
-
 module.exports = router;
