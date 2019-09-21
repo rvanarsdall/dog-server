a8f7bcce25846309d59233975f7dc0fc5e4634a0
robert vanarsdall <robv@roberts-MacBook-Pro.local>
2019-09-21 10:41:03 -0400

    fixed postgress

--- db.js
+++ db.js
@@ -1,14 +1,20 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: 'postgres',}) 
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgresql://postgres:${encodeURIComponent(
+      process.env.PASS
+    )}@localhost/doggo`,
+  {
+    dialect: "postgres"
+  }
+);
+sequelize.authenticate().then(
+  function() {
+    console.log("Connected to  postgres database!");
+  },
+  function(err) {
+    console.log(err);
+  }
+);
 
-sequelize.authenticate()
-  .then(
-    function(){
-      console.log('Connected to  postgres database!')
-    }, 
-    function(err){
-      console.log(err);
-    }
-  )
-
-  module.exports = sequelize;
+module.exports = sequelize;
